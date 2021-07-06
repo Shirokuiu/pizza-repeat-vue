16ab5efaf4158b28435fcb56f62fad8dd290a990
Павел Осипов <pavelosipov@iMac-Pavel.local>
2021-07-06 21:55:02 +0300

    Подключил роутер и добавил первый путь

--- src/frontend/src/layouts/AppLayout.vue
+++ src/frontend/src/layouts/AppLayout.vue
@@ -2,19 +2,16 @@
   <div>
     <TheHeader />
 
-    <Index />
+    <router-view />
   </div>
 </template>
 <script>
 import TheHeader from "../modules/header/components/TheHeader";
-import Index from "../views/Index";
-
 export default {
   name: "AppLayout",
 
   components: {
     TheHeader,
-    Index,
   },
 };
 </script>
