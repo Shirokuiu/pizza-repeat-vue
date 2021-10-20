48a068a9a2cf7c8b5549e992781d7d934c75f947
 Pavel <pavel@iMac-Pavel.local>
2021-10-20 13:23:07 +0300

    Finish lesson

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <Index></Index>
+  <div class="app__wrap">
+    <TheHeader />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import Index from "./views/Index";
+import TheHeader from "@/modules/header/components/TheHeader";
 
 export default {
   name: "App",
   components: {
-    Index,
+    TheHeader,
   },
 };
 </script>
 
 <style lang="scss">
 @import "~@/assets/scss/app";
+
+// NOTE: Иначе едет верстка в корзине
+.app__wrap {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
 </style>
